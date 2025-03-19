19ca52d6d78be342f860fe0ca9b480d988ad6b18
Peter Amorese <peter.amorese@gmail.com>
2025-03-19 12:21:52 -0600

    Coded, but not working

--- experiments/integral.py
+++ experiments/integral.py
@@ -22,16 +22,16 @@ def std_gaussian_cdf_deriv(x):
 ## Initial state and noise distribution cdf and quantiles ##
 
 def Cx0(x):
-    return [std_gaussian_cdf(x[0]), std_gaussian_cdf(x[1])]
+    return (std_gaussian_cdf(x[0]), std_gaussian_cdf(x[1]))
 
 def Qx0(yx):
-    return [std_gaussian_quantile(yx[0]), std_gaussian_quantile(yx[0])]
+    return (std_gaussian_quantile(yx[0]), std_gaussian_quantile(yx[0]))
 
 def Cw(w):
-    return [std_gaussian_cdf(w[0]), std_gaussian_cdf(w[1])]
+    return (std_gaussian_cdf(w[0]), std_gaussian_cdf(w[1]))
 
 def Qw(yw):
-    return [std_gaussian_quantile(yw[0]), std_gaussian_quantile(yw[0])]
+    return (std_gaussian_quantile(yw[0]), std_gaussian_quantile(yw[0]))
 
 ## Dynamics and the inverse dynamics ##
 
@@ -42,43 +42,50 @@ def g(w, x):
     def g1(x, w):
         return w[0]**3 * 0.5 * x[1] + 1/10 * x[0]**4 + 10 * np.sin(2 * x[0])
 
-    return [g0(x, w), g1(x, w)]
+    return (g0(x, w), g1(x, w))
 
 def g_inv(w, xp):
     def g0_inv(w, xp):
-        return (xp[0] - w[1]**2)**(1/3)
+        a = xp[0] - w[1]**2
+        return np.sign(a) * (np.abs(a))**(1/3)
 
     def g1_inv(w, xp):
-        return -2 / (w[0]**3) * xp[1] - (1/10 * g0_inv(w, xp)**4 + 10 * np.sin(2 * g0_inv(w, xp)))
+        return -2 / (w[0]**3) * (xp[1] - 1/10 * g0_inv(w, xp)**4 + 10 * np.sin(2 * g0_inv(w, xp)))
 
-    return [g0_inv(w, xp), g1_inv(w, xp)]
+    return (g0_inv(w, xp), g1_inv(w, xp))
 
 ## Transformation functions to the uniform domain ##
 
 def Phi(w, x0):
-    return [Cw(w), Cx0(x0)]
+    return (Cw(w), Cx0(x0))
 
 def Phi_inv(yw, yx):
-    return [Cw(yw), Qx0(yx)]
+    return (Cw(yw), Qx0(yx))
 
 ## Flow Functions ##
+
+def G(w, x):
+    return (w, g(x))
+
+def G_inv(w, xp):
+    return (w, g_inv(w, xp))
 
 def Gok(w, x0, k):
     xi = x0
     for i in range(0, k):
         xi = g(w, xi)
-    return [w, xi]
+    return (w, xi)
 
 def Gok_inv(w, xk, k):
     xi = xk
     for i in range(k, 0, -1):
         xi = g_inv(w, xi)
-    return [w, xi]
+    return (w, xi)
 
 ## Jacobians ##
 
 def J_G(w, x):
-    J = np.zeros(n + m, n + m)
+    J = np.zeros((n + m, n + m))
     
     # dw0/dw0
     J[0, 0] = 1
@@ -112,8 +119,8 @@ def J_G(w, x):
 
     return J
 
-def J_Phi(yw, yx):
-    J = np.zeros(n + m, n + m)
+def J_Phiinv(yw, yx):
+    J = np.zeros((n + m, n + m))
 
     # dw0/dyw0
     J[0, 0] = std_gaussian_quantile_deriv(yw[0])
@@ -141,20 +148,49 @@ def J_Phi(yw, yx):
 
     return J
 
+# For now use matrix inverse cus im lazy
+def J_Ginv(w, xp):
+    return np.linalg.inv(J_G(*G_inv(w, xp)))
+
+# For now use matrix inverse cus im lazy
+def J_Phi(w, x0):
+    return np.linalg.inv(J_Phiinv(*Phi(w, x0)))
+
+## Density at time k ##
+
+def p(w, xk, k):
+    # Invert to x0
+    J = np.identity(n + m)
+    for i in range(k, 0, -1):
+        J = np.matmul(J, J_Ginv(*Gok_inv(w, xk, i - 1)))
+
+    # Invert to y
+    J = np.matmul(J, J_Phi(*Gok_inv(w, xk, k)))
+
+    return np.abs(np.linalg.det(J))
+
 ## Plot the distributions
 
-def plot_state_dist(ax, k : int, resolution : int, x_bounds : np.ndarray, w_bounds : np.ndarray):
-    Xk0, Xk1, W0, W1 = np.meshgrid(np.linspace(x_bounds[0], x_bounds[1], resolution), 
-                                 np.linspace(x_bounds[2], x_bounds[3], resolution), 
-                                 np.linspace(w_bounds[0], w_bounds[1], resolution), 
-                                 np.linspace(w_bounds[2], w_bounds[3], resolution))
+def plot_state_dist(ax :plt.Axes, k : int, resolution : int, x_bounds : np.ndarray, w_bounds : np.ndarray):
+    test = p((0, 0), (0, 0), 2)
+    #W0, W1, Xk0, Xk1 = np.meshgrid(np.linspace(x_bounds[0], x_bounds[1], resolution), 
+    #                             np.linspace(x_bounds[2], x_bounds[3], resolution), 
+    #                             np.linspace(w_bounds[0], w_bounds[1], resolution), 
+    #                             np.linspace(w_bounds[2], w_bounds[3], resolution), indexing='ij')
+    
+    #p_values_w_x = np.vectorize(lambda w0, w1, xk0, xk1: p((w0, w1), (xk0, xk1), k))(W0, W1, Xk0, Xk1)
+
+    #p_values_x = np.sum(p_values_w_x, axis=(0, 1))
+
+    #ax.contourf(Xk0, Xk1, p_values_x.T, levels=50, cmap='viridis')
+    #ax.colorbar(label='p(x0, x1)')
     
 
 
 def main():
-    n = 2
-    m = 2
-
+    fig = plt.figure()
+    ax = fig.gca()
+    plot_state_dist(ax, 2, 100, [-3, 3, -3, 3], [-100, 100, -100, 100])
 
 if __name__ == "__main__":
     main()
